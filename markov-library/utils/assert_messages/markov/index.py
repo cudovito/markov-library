b48d120a9977fd36687cab98ed0d5ebd78b32d05
Rishabh Bhardwaj <rishabh@swifterhq.com>
2021-04-02 16:25:54 +0530

    assert statements added

---
+++ markov-library/utils/assert_messages/markov/index.py
@@ -0,0 +1,10 @@
+markov_assert = {
+    # Type check
+    'DATA_SET_TYPE_CHECK': 'Type of arg "data_set" must be dataframe',
+    'COLUMN_1_TYPE_CHECK': 'Type of arg "column_1" must be string', 
+    'COLUMN_2_TYPE_CHECK':  'Type of arg "column_2" must be string', 
+
+    # Null value check
+    'COLUMN_1_NULL_CHECK': 'Value of arg "column_1" cannot be null', 
+    'COLUMN_2_NULL_CHECK': 'Value of arg "column_2" cannot be null', 
+}
--- markov-library/utils/unique.py
+++ markov-library/utils/unique.py
@@ -1,3 +1,6 @@
+# Assert messages
+from assert_messages.unique.index import unique_assert
+
 # Get unique elements from a list
 class Unique:
     
@@ -10,7 +13,11 @@ class Unique:
         Input: list1(List): Contains the list of the elements
         Output: unique_list(list) 
         """
-        
+
+        # Args validation
+        assert isinstance(list1, list), unique_assert.get('LIST1_TYPE_CHECK')
+        assert isinstance(list1[0], float), unique_assert.get('LIST1_DIMENSION_CHECK')
+
         # Variable declaration  
         unique_list = []
 
@@ -21,4 +28,4 @@ class Unique:
                 # If value not in list append
                 unique_list.append(x)
 
-        return (unique_list)
+        return unique_list
